158450ff0da3e913a313165879cc21e7bf7d9d95
uyriq <105006434+uyriq@users.noreply.github.com>
2022-05-19 22:44:21 +0300

    WithFetchFilm = withFetch()

--- src/components/app.js
+++ src/components/app.js
@@ -1,39 +1,13 @@
 import React from "react";
-import Film from "./film";
-import styles from "../styles.module.css";
+import withFetch from "../hocs/with-fetch"
 
 export default class App extends React.Component {
-  state = {
-    isLoading: false,
-    hasError: false,
-          data: []
-  }
   
-  componentDidMount() {
-    this.getFilms();
-  }
-
-  getFilms = () => {
-    this.setState({ ...this.state, hasError: false, isLoading: true });
-    fetch("https://api.nomoreparties.co/beatfilm-movies")
-      .then((res) => res.json())
-      .then((data) => this.setState({ ...this.state, data, isLoading: false }))
-      .catch((e) => {
-        this.setState({ ...this.state, hasError: true, isLoading: false });
-      });
-  };
+  const WithFetchFilm = withFetch(); 
 
   render() {
-    const { data, isLoading, hasError } = this.state;
     return (
-      <div className={`${styles.app} ${styles.grid}`}>
-        {isLoading && "Загрузка..."}
-        {hasError && "Произошла ошибка"}
-        {!isLoading &&
-          !hasError &&
-          data.length &&
-          data.map((film, index) => <Film key={index} data={film} />)}
-      </div>
+      <WithFetchFilm />
     );
   }
 }
---
+++ src/components/hocs/with-fetch.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Film from '../film';
+import styles from '../../styles.module.css'
+
+export default const withFetch = (/* WrappedComponent */) => {
+     return class extends React.Component { 
+        constructor(props) {
+             super(props); 
+            // this.onButtonClick = this.onButtonClick.bind(this);
+            // this.state = { isToggled: this.props.initialFetchState };
+            state = {
+                isLoading: false,
+                hasError: false,
+                      data: []
+              }
+        }  
+              componentDidMount() {
+                this.getFilms();
+              }
+            
+              getFilms = () => {
+                this.setState({ ...this.state, hasError: false, isLoading: true });
+                fetch("https://api.nomoreparties.co/beatfilm-movies")
+                  .then((res) => res.json())
+                  .then((data) => this.setState({ ...this.state, data, isLoading: false }))
+                  .catch((e) => {
+                    this.setState({ ...this.state, hasError: true, isLoading: false });
+                  });
+              };
+            
+              render() {
+                const { data, isLoading, hasError } = this.state;
+                return (
+                  <div className={`${styles.app} ${styles.grid}`}>
+                    {isLoading && "Загрузка..."}
+                    {hasError && "Произошла ошибка"}
+                    {!isLoading &&
+                      !hasError &&
+                      data.length &&
+                      data.map((film, index) => <Film key={index} data={film} />)}
+                  </div>
+                );
+              }
+            }
+        }
